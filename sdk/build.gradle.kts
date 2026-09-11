029dd47e7e938a3502fc4f015836ec9fc85d2eaf
Martin Kellner <martin.kellner@corbado.com>
2026-09-11 16:46:25 +0200

    v1.4.1

--- sdk/build.gradle.kts
+++ sdk/build.gradle.kts
@@ -59,7 +59,7 @@ mavenPublishing {
     publishToMavenCentral()
     signAllPublications()
 
-    coordinates("com.corbado", "connect-core", "1.4.0")
+    coordinates("com.corbado", "connect-core", "1.4.1")
 
     pom {
         name.set("Corbado Connect SDK")
